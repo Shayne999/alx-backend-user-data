01bb4791ca0098a8fd0dfc76ed44b7236df9c216
Shayne999 <shaynejuniorn@gmail.com>
2024-08-13 15:24:04 +0200

    Update password route clean up

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -98,15 +98,13 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    is_password_changed = False
+
     try:
         AUTH.update_password(reset_token, new_password)
-        is_password_changed = True
     except ValueError:
-        is_password_changed = False
-    if not is_password_changed:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
